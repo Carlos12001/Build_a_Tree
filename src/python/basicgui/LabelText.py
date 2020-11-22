bff8118f5c6655c09597c2abfef4e55258ae4efc
Carlos12001 <carlos.andres12001@gmail.com>
2020-11-22 13:02:08 -0600

    textField

--- src/python/basicgui/LabelText.py
+++ src/python/basicgui/LabelText.py
@@ -6,7 +6,7 @@ from pygame.ftfont import Font
 pygame.font.init()
 
 
-class Text:
+class LabelText:
     __fonts: List[Union[None, Font, FontType]] = [
         pygame.font.SysFont("Times New Roman", 70),
         pygame.font.SysFont("Times New Roman", 20),
@@ -23,9 +23,15 @@ class Text:
         self.__color: Tuple[int] = color
         self.__surface: pygame.Surface = surface
         self.__position: List[int] = position
-        self.txtObj: Optional[Any] = (Text.__fonts[self.__type_font]).render(self.__text, 1, self.__color)
+        self.txtObj: Optional[Any] = (LabelText.__fonts[self.__type_font]).render(self.__text, 1, self.__color)
         self.txtRect = self.txtObj.get_rect()
-        self.txtRect.center = self.__position
+        self.draw_me()
 
     def draw_me(self):
         self.__surface.blit(self.txtObj, self.txtRect)
+
+    def set_text(self, text: str):
+        self.__text: str = text
+        self.txtObj: Optional[Any] = (LabelText.__fonts[self.__type_font]).render(self.__text, 1, self.__color)
+        self.txtRect = self.txtObj.get_rect()
+        self.txtRect.center = self.__position
--- src/python/screen/SceneGame.py
+++ src/python/screen/SceneGame.py
@@ -5,7 +5,7 @@ import pygame
 from pygame.surface import Surface
 import python.basicgui.Button
 from python.basicgui.CursorRect import CursorRect
-from python.basicgui.LabelText import Text
+from python.basicgui.LabelText import LabelText
 
 
 class SceneGame:
@@ -23,12 +23,15 @@ class SceneGame:
    -------
 
    """
+
     # Static Attributes
     __path_game: str = os.getcwd()[0:len(os.getcwd()) - 5]
     __colors: dict = {"black": (0, 0, 0),
-                                       "cyan": (14, 190, 187),
-                                       "red_light": (190, 74, 71),
-                                       "cyan_light": (178, 223, 219)}
+                      "cyan": (14, 190, 187),
+                      "red_light": (190, 74, 71),
+                      "cyan_light": (178, 223, 219),
+                      "brown": (141,137,128),
+                      "white": (255, 255, 255)}
 
     def __init__(self) -> None:
         """
@@ -48,14 +51,22 @@ class SceneGame:
 
         bg_image = SceneGame.load_out_img("background.png", (self.__scene_size_X, self.__scene_size_Y))
         cursorRect = CursorRect()
-        label_tittle: Text = Text("Build a Tree", 0, SceneGame.get_color()["black"], self.__screen, (500, 50))
+
+        label_text_title: LabelText = LabelText("Build a Tree", 0, SceneGame.get_color()["black"], self.__screen, (500, 50))
+
+        active_text_field = False
+        text_field = pygame.Rect(300, 350, 400, 50)
+        label_text_indication = LabelText("Ingrese su nombre", 1,SceneGame.get_color()["red_light"], self.__screen, (500, 200))
+        user_name = ""
+        label_text_text_field = LabelText(user_name, 1, SceneGame.get_color()["white"], self.__screen, (text_field.x + 200, text_field.y + 25))
+
         button_start = python.basicgui.Button.Button(SceneGame.load_out_img("buttonStartNormal.png", (150, 100)),
                                                      SceneGame.load_out_img("buttonStartSelection.png", (150, 100)),
                                                      (500, 600), self.__screen)
+
         run = True
         while run:
             self.__screen.blit(bg_image, [0, 0])
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -63,10 +74,30 @@ class SceneGame:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if button_start.get_rect().collidepoint(event.pos):
                         print("HI")
-            cursorRect.update()
-            label_tittle.draw_me()
-            button_start.update(cursorRect)
+                        active_text_field = True
+                        # run = False
+                    elif text_field.collidepoint(event.pos):
+                        active_text_field = True
+                    else:
+                        active_text_field = False
+                elif event.type == pygame.KEYDOWN:
+                    if active_text_field:
+                        if event.key == pygame.K_BACKSPACE:
+                            user_name = user_name [0:-1]
+                            label_text_text_field.set_text(user_name)
+                        elif event.key == pygame.K_TAB:
+                            user_name = user_name
+                            label_text_text_field.set_text(user_name)
+                        else:
+                            user_name += event.unicode
+                            label_text_text_field.set_text(user_name)
 
+            cursorRect.update()
+            label_text_title.draw_me()
+            button_start.update(cursorRect)
+            pygame.draw.rect(self.__screen, SceneGame.get_color()["brown"], text_field)
+            label_text_indication.draw_me()
+            label_text_text_field.draw_me()
             pygame.display.flip()
 
     @staticmethod
