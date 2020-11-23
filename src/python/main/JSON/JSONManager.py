24404ff84cf1afbcd7e0d18251ecbfe0b57003c4
mauro <mauronav006@gmail.com>
2020-11-23 01:09:46 -0600

    Classes for Json managment
    Couple of classes created in order to exchange information through objects and Json strings. JSon Manager : Switches between DIctionaries and JSON string. 
    UpdateInfo: Sends or receives info, its the mold of the message

---
+++ src/python/main/JSON/JSONManager.py
@@ -0,0 +1,36 @@
+import json
+
+
+class JSONManager(object):
+
+    def __init__(self):
+        self.info = None
+
+    def selectAction(self, info):
+
+        if (type(info) == str):
+            self.info = info
+            return self.decoder()
+        elif (type(info) == dict):
+            self.info = info
+            return self.encoder()
+        else:
+            print("Error: The info's type is not valid")
+
+    def decoder(self):
+        print(type(self.info))
+        dictElem = json.loads(self.info)
+        print(type(dictElem))
+        print('dict:', dictElem)
+        return dictElem
+
+    def encoder(self):
+        print(type(self.info))
+        jsonStr = json.dumps(self.info)
+        print(type(jsonStr))
+        print("str: ", jsonStr)
+        return jsonStr
+
+
+#manager = JSONManager("e", '{"name": "Bob", "languages": "English"}')
+#manager.selectAction()
