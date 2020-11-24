e4042bd2fa4c8fed2216db0faaa9e447abe6af72
Carlos12001 <carlos.andres12001@gmail.com>
2020-11-24 07:59:41 -0600

    awds

--- src/javathings/main/Server.java
+++ src/javathings/main/Server.java
@@ -12,7 +12,7 @@ public class Server {
     public static void main(String[] args){
         javathings.conection.ServerConnection server = javathings.conection.ServerConnection.getInstance();
         server.listenSocket();
-        javathings.gui.ServerGUI frame = new javathings.gui.ServerGUI(server.getPort());
+        new javathings.gui.ServerGUI(server.getPort());
 
         //server.writeSocket("soy el server de java");
     }
