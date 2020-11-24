ef0c7a2a42fc36be0e45e623436020fe4f0fdece
Carlos12001 <carlos.andres12001@gmail.com>
2020-11-23 21:46:04 -0600

    Update StaticState.py

--- src/python/sprites/StaticState.py
+++ src/python/sprites/StaticState.py
@@ -5,7 +5,6 @@ from python.sprites.EntitySate import EntityState
 
 class StaticState(EntityState):
 
-
     def __init__(self, image, name):
         super(StaticState, self).__init__()
         self.set_current_sprite(image)
