2c0baefe734e6d7bb2620bae9c73265ad931fd25
Apollo13 <calderon.ig08@estudiantec.cr>
2020-12-02 01:13:45 -0600

    Fix Avl

--- src/javathings/trees/AVL/TreeAVL.java
+++ src/javathings/trees/AVL/TreeAVL.java
@@ -9,15 +9,16 @@ import org.w3c.dom.Node;
  */
 public class TreeAVL extends Tree {
 
-    public static void main(String[] args) {
-    TreeAVL arbol = new TreeAVL();
-    arbol.insert(3);
-    arbol.insert(5);
+//    public static void main(String[] args) {
+//    TreeAVL arbol = new TreeAVL();
+//    arbol.insert(3);
+//    arbol.insert(5);
 //    arbol.insert(1);
 //    arbol.insert(34);
 //    arbol.insert(0);
 //    System.out.println(arbol.contains(34));
-    }
+//    }
+
     /**
      *
      */
@@ -28,22 +29,25 @@ public class TreeAVL extends Tree {
     }
 
     public boolean contains(int element) {
-        return this.contains(element, (NodeBST) this.root);
+        return this.contains(element, (NodeAVL) this.root);
     }
 
-    private boolean contains(int element, NodeBST node) {
+    private boolean contains(int element, NodeAVL node) {
         if (node == null) {
             return false;
         } else {
             if (element < node.getToken())
-                return contains(element, (NodeBST) node.getLeft());
+                return contains(element, (NodeAVL) node.getLeft());
             else if (element > node.getToken())
-                return contains(element, (NodeBST) node.getRight());
+                return contains(element, (NodeAVL) node.getRight());
             else
                 return true;
         }
     }
 
+    /**
+     * @param key
+     */
     public void insert(int key){
         this.root = insert(key, (NodeAVL) this.root);
     }
@@ -65,30 +69,46 @@ public class TreeAVL extends Tree {
         }
         tree.setHeight(1 + Math.max(height((NodeAVL) tree.getLeft()), height((NodeAVL) tree.getRight())));
 
-        return balance(tree);
+        return balance(tree, key);
     }
 
+    /**
+     * @param N
+     * @return
+     */
+    private int getBalance(NodeAVL N) {
+        if (N == null)
+            return 0;
+        return height((NodeAVL) N.getLeft()) - height((NodeAVL) N.getRight());
+    }
     /**
      * @param tree
      * @return
      */
-    private NodeAVL balance(NodeAVL tree) {
-        if (tree == null) {
-            return tree;
-        } else if (height((NodeAVL) tree.getLeft()) - height((NodeAVL) tree.getRight()) > 1) { // 1 es la diferencia permitida
-            if (height((NodeAVL) tree.getLeft().getLeft()) >= height((NodeAVL) tree.getLeft().getRight()))
-                tree = rotateLeft(tree);
-            else
-                tree = doubleLeftChild(tree);
-        } else {
-            if (height((NodeAVL) tree.getRight()) - height((NodeAVL) tree.getLeft()) > 1) {
-                if (height((NodeAVL) tree.getRight().getRight()) >= height((NodeAVL) tree.getRight().getLeft()))
-                    tree = rotateRight(tree);
-            } else {
-                    tree = doubleRightChild(tree);
-                }
-            }
-        tree.setHeight(Math.max(height((NodeAVL) tree.getLeft()), height((NodeAVL) tree.getRight())) + 1);
+    private NodeAVL balance(NodeAVL tree, int key) {
+
+        int balance = getBalance(tree);
+
+        if (balance > 1 && key < tree.getLeft().getToken())
+            return rotateRight(tree);
+
+        // Right Right Case
+        if (balance < -1 && key > tree.getRight().getToken())
+            return rotateLeft(tree);
+
+        // Left Right Case
+        if (balance > 1 && key > tree.getLeft().getToken()) {
+            tree.setLeft(rotateLeft((NodeAVL) tree.getLeft()));
+            return rotateRight(tree);
+        }
+
+        // Right Left Case
+        if (balance < -1 && key < tree.getRight().getToken()) {
+            tree.setRight(rotateRight((NodeAVL) tree.getRight()));
+            return rotateLeft(tree);
+        }
+
+        /* return the (unchanged) node pointer */
         return tree;
     }
 
@@ -123,24 +143,6 @@ public class TreeAVL extends Tree {
         root.setHeight(Math.max(height((NodeAVL) root.getLeft()), height((NodeAVL) root.getRight())) + 1);
         x.setHeight(Math.max(height((NodeAVL) x.getLeft()), x.getHeight()) + 1);
         return x;
-    }
-
-    /**
-     * @param child
-     * @return
-     */
-    private NodeAVL doubleLeftChild(NodeAVL child) {
-        child.setLeft(rotateRight((NodeAVL) child.getLeft()));
-        return rotateLeft(child);
-    }
-
-    /**
-     * @param child
-     * @return
-     */
-    private NodeAVL doubleRightChild(NodeAVL child) {
-        child.setLeft(rotateRight((NodeAVL) child.getLeft()));
-        return rotateLeft(child);
     }
 
     /**
